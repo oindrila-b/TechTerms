c188fed6ac138bab52961b73030bf10f36858d38
oindrilabanerjee <banerjee.oindrila.ob@gmail.com>
2023-12-05 01:09:10 +0530

    New Changes

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -17,45 +17,8 @@ import { Card } from 'react-bootstrap';
 
 export const Home = () => {
     return (
-        <div className='banner'>
-            <div className="computer">
-                <div className="computer-image">
-                    <img src={image} alt="" width={"100%"} />
-                </div>
-                {/* <h1 className='title'>Tech Terms</h1> */}
-                <div className="circle">
-                    <div className='icon'><img src={microchip} /></div>
-                    <div className='icon'><img src={database} /></div>
-                    <div className='icon'><img src={userRobot} /></div>
-                    <div className='icon'><img src={laptopmobile} /></div>
-                    <div className='icon'><img src={cloudShare} /></div>
-                    <div className='icon'><img src={headVr} /></div>
-                    <div className='icon'><img src={qrcode} /></div>
-                    <div className='icon'><img src={unity} /></div>
-                </div>
-                <div className="asteroid">
-                    <div className="stone"></div>
-                    <div className="stone"></div>
-                    <div className="stone"></div>
-                    <div className="stone"></div>
-                    <div className="stone"></div>
-                    <div className="stone"></div>
-                    <div className="stone"></div>
-                </div>
-            </div>
-            <HashLink smooth to='/home/#categories' >
-                <div>
-                    <img src={down} alt="" width={"2%"} />
-                    {/* <h2 className='see-more'>See More</h2> */}
-                </div>
-            </HashLink>
-            <div id='categories'>
-            <h1 className='see-more'>CATEGORIES</h1>
-            <Card style={{width:'18rem'}}>
-                <Card.Img variant='top' src='' />
-            </Card>
-            </div>
-
+        <div>
+            <h1>Tech Terms</h1>
         </div>
     )
 }
